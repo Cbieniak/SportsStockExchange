15f7b826b7b9a168b32d4dc340f6036655743fdb
Christian Bieniak <cbieniak@gmail.com>
2018-05-20 22:34:50 +1000

    Store local host in a temp struct until I figure out environments

---
+++ Sources/App/Constants/DevelopmentConstants.swift
@@ -0,0 +1,12 @@
+//
+//  DevelopmentConstants.swift
+//  App
+//
+//  Created by Christian Bieniak on 20/5/18.
+//
+
+import Vapor
+
+struct DevelopmentConstants {
+    static var developmentURL = "http://localhost:8080"
+}
